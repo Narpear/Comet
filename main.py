7536037c9cb3ba0c08b19a8aebae81c9dda9b3ed
Narpear <prer.kulk@gmail.com>
2024-04-26 19:27:02 +0530

    debugging in progress

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+# main.py
 import discord
 from discord.ext import commands
-import music
+
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix='.', intents=intents)
 
 cogs = [music]
-client = commands.Bot(command_prefix='.', intents=discord.Intents.all())
 
 for i in range(len(cogs)):
-  cogs[i].setup(client)
+    cogs[i].setup(client)
 
-client.run(
-    "MTIzMzM1MTg3OTAwNjYyMTcyNw.GigoWx.ncdpWj2v3r-UKWnRYMC06xS9RSutA4Pni-bSYk")
+
+client.run("MTIzMzM1MTg3OTAwNjYyMTcyNw.GigoWx.ncdpWj2v3r-UKWnRYMC06xS9RSutA4Pni-bSYk")
